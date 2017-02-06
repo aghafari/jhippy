1ae582147e715319302fb8514b2f3787b1722d12
Ashkan Ghafari <ashkanghaffari@yahoo.com>
2017-02-06 12:59:11 -0800

    - first commit

---
+++ src/main/webapp/app/admin/admin.state.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('jhippyApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig ($stateProvider) {
+        $stateProvider.state('admin', {
+            abstract: true,
+            parent: 'app'
+        });
+    }
+})();
